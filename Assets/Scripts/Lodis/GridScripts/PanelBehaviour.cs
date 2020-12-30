04e4139c8c8a655203663ef30cd46ff6f2a96836
lodis <lodisperkins44@gmail.com>
2020-12-29 18:38:03 -0600

    Added blackboard. Gameobjects can now move on the grid using GridMovementBehaviour

---
+++ Assets/Scripts/Lodis/GridScripts/PanelBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lodis.GridScripts
+{
+    public class PanelBehaviour : MonoBehaviour
+    {
+        private Vector2 _position;
+        private bool _occupied;
+
+        /// <summary>
+        /// The position of this panel on the grid.
+        /// </summary>
+        public Vector2 Position
+        {
+            get
+            {
+                return _position;
+            }
+            set
+            {
+                _position = value;
+            }
+        }
+
+        /// <summary>
+        /// Returns if there is anything preventing an object from moving on to this panel. READ ONLY
+        /// </summary>
+        public bool Occupied
+        {
+            get
+            {
+                return _occupied;
+            }
+        }
+    }
+}
+
+
