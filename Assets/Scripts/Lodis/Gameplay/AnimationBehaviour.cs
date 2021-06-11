c1c629fd30b991432e7a66a17c65f79296de94e7
lodisperkins <lodisperkins44@gmail.com>
2021-06-11 17:00:36 -0500

    Began custom animator set up

--- Assets/Scripts/Lodis/Gameplay/AnimationBehaviour.cs
+++ Assets/Scripts/Lodis/Gameplay/AnimationBehaviour.cs
@@ -1,19 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 namespace Lodis.Gameplay
 {
+    enum AnimationPhase
+    {
+        STARTUP,
+        ACTIVE,
+        INACTIVE
+    }
+
     public class AnimationBehaviour : MonoBehaviour
     {
         [SerializeField]
         private Movement.GridMovementBehaviour _moveBehaviour;
         [SerializeField]
         private Animator _animator;
+        private Ability _currentAbilityAnimating;
+        [SerializeField]
+        private RuntimeAnimatorController _runtimeAnimator;
 
         // Start is called before the first frame update
         void Start()
         {
+        }
+
+        private float CalculateAnimationSpeed(AnimationClip animation, AnimationPhase phase)
+        {
+            switch (phase)
+            {
+                case AnimationPhase.STARTUP:
+                        return (_animator.speed * _currentAbilityAnimating.abilityData.startUpTime) / animation.events[0].time;
+                case AnimationPhase.ACTIVE:
+                    return (_animator.speed * _currentAbilityAnimating.abilityData.timeActive) / animation.events[1].time - animation.events[0].time;
+                case AnimationPhase.INACTIVE:
+                    return (_animator.speed * _currentAbilityAnimating.abilityData.recoverTime) / animation.length - animation.events[1].time;
+            }
+
+            return 1;
+        }
+
+        public void PlayAbilityAnimation(Ability ability)
+        {
+            AnimationClip animation;
+            if (!ability.abilityData.GetCustomAnimation(out animation))
+                return;
 
         }
 
