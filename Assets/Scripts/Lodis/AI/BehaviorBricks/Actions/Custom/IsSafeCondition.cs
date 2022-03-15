c673c4d44b0ee533e0aafcdb02635b1ddaed3395
lodisperkins <lodisperkins44@gmail.com>
2022-03-15 18:23:24 -0500

    Added custom conditions

---
+++ Assets/Scripts/Lodis/AI/BehaviorBricks/Actions/Custom/IsSafeCondition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pada1.BBCore;           // Code attributes
+using Pada1.BBCore.Framework; // ConditionBase
+
+[Condition("CustomConditions/IsSafe")]
+public class IsSafeCondition : ConditionBase
+{
+    public override bool Check()
+    {
+        throw new System.NotImplementedException();
+    }
+}
