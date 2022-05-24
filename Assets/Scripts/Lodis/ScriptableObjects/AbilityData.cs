873afda17d4ddf9fc6c30d224e2eff9d58684105
lodisperkins <lodisperkins44@gmail.com>
2022-05-23 20:02:15 -0500

    Players can block any ability thats not unblockable

--- Assets/Scripts/Lodis/ScriptableObjects/AbilityData.cs
+++ Assets/Scripts/Lodis/ScriptableObjects/AbilityData.cs
@@ -120,7 +120,7 @@ namespace Lodis.ScriptableObjects
                 Debug.LogWarning("GetColliderInfo() was called with an invalid index passed as a parameter");
                 return new HitColliderInfo();
             }
-
+            ColliderData[index].AbilityType = AbilityType;
             return ColliderData[index];
         }
 
