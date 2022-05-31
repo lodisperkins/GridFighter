38130ad8046f22f7f7331ae8d3df49a9bc654d74
lodisperkins <lodisperkins44@gmail.com>
2022-05-30 21:20:29 -0500

    Fixed cel shader

--- Assets/Shaders/GetLighting.hlsl
+++ Assets/Shaders/GetLighting.hlsl
@@ -1,20 +1,40 @@
-﻿void MainLight_half(float3 WorldPos, out half3 Direction, out half3 Color, out half Attenuation)
+﻿void MainLight_half(half3 WorldPos, out half3 Direction, out half3 Color,
+	out half DistanceAttenuation, out half ShadowAttenuation)
 {
-#if SHADERGRAPH_PREVIEW
-	Direction = half3(0.5, 0.5, 0);
-	Color = 1;
-	Attenuation = 1;
-#else
-#if SHADOWS_SCREEN
-	half4 clipPos = TransformWorldToHClip(WorldPos);
-	half4 shadowCoord = ComputeScreenPos(clipPos);
+#ifdef SHADERGRAPH_PREVIEW
+	Direction = normalize(half3(0.5f, 0.5f, 0.25f));
+	Color = half3(1.0f, 1.0f, 1.0f);
+	DistanceAttenuation = 1.0f;
+	ShadowAttenuation = 1.0f;
 #else
 	half4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
-#endif
-
 	Light mainLight = GetMainLight(shadowCoord);
+
 	Direction = mainLight.direction;
 	Color = mainLight.color;
-	Attenuation = mainLight.distanceAttenuation * mainLight.shadowAttenuation;
+	DistanceAttenuation = mainLight.distanceAttenuation;
+	ShadowAttenuation = mainLight.shadowAttenuation;
+#endif
+}
+
+void AdditionalLight_half(half3 WorldPos, int Index, out half3 Direction,
+	out half3 Color, out half DistanceAttenuation, out half ShadowAttenuation)
+{
+	Direction = normalize(half3(0.5f, 0.5f, 0.25f));
+	Color = half3(0.0f, 0.0f, 0.0f);
+	DistanceAttenuation = 0.0f;
+	ShadowAttenuation = 0.0f;
+
+#ifndef SHADERGRAPH_PREVIEW
+	int pixelLightCount = GetAdditionalLightsCount();
+	if (Index < pixelLightCount)
+	{
+		Light light = GetAdditionalLight(Index, WorldPos);
+
+		Direction = light.direction;
+		Color = light.color;
+		DistanceAttenuation = light.distanceAttenuation;
+		ShadowAttenuation = light.shadowAttenuation;
+	}
 #endif
 }
