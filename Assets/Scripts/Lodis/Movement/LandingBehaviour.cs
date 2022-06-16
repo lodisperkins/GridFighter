f672eeeee5653a91c542e384256a581502ce549e
lodisperkins <lodisperkins44@gmail.com>
2022-06-16 09:22:31 -0500

    Added boolean to prevent frequent landing checks

--- Assets/Scripts/Lodis/Movement/LandingBehaviour.cs
+++ Assets/Scripts/Lodis/Movement/LandingBehaviour.cs
@@ -16,6 +16,7 @@ namespace Lodis.Movement
         [SerializeField] private float _knockDownRecoverInvincibleTime;
         [SerializeField] private float _knockDownLandingTime;
         private KnockbackBehaviour _knockback;
+        private bool CanCheckLanding;
 
         public float LandingTime { get => _landingTime;}
         public bool IsDown { get; private set; }
@@ -44,16 +45,26 @@ namespace Lodis.Movement
             _knockback.AddOnKnockBackAction(() => Landing = false);
         }
 
-        private void OnCollisionStay(Collision collision)
+        private void OnCollisionEnter(Collision collision)
         {
             if (_knockback.CheckIfIdle()) return;
 
             if (collision.gameObject.CompareTag("CollisionPlane") && (_knockback.CurrentAirState == AirState.TUMBLING || _knockback.CurrentAirState == AirState.FREEFALL))
             {
-                TryStartLandingLag();
+                CanCheckLanding = true;
             }
         }
-    
+
+        private void OnCollisionExit(Collision other)
+        {
+            if (_knockback.CheckIfIdle()) return;
+
+            if (other.gameObject.CompareTag("CollisionPlane") && (_knockback.CurrentAirState == AirState.TUMBLING || _knockback.CurrentAirState == AirState.FREEFALL))
+            {
+                CanCheckLanding = false;
+            }
+        }
+
         private void CancelLanding()
         {
             if (!Landing) return;
@@ -98,7 +109,11 @@ namespace Lodis.Movement
             IsDown = false;
             //Start recovery from knock down
             _knockback.Physics.MakeKinematic();
-            _landingAction = RoutineBehaviour.Instance.StartNewTimedAction(values => RecoveringFromFall = false,
+            _landingAction = RoutineBehaviour.Instance.StartNewTimedAction(values =>
+                {
+                    RecoveringFromFall = false;
+                    CanCheckLanding = false;
+                },
                 TimedActionCountType.SCALEDTIME, KnockDownRecoverTime);
         }
         
@@ -118,7 +133,11 @@ namespace Lodis.Movement
                     break;
                 case AirState.FREEFALL:
                     _knockback.CurrentAirState = AirState.NONE;
-                    _landingAction = RoutineBehaviour.Instance.StartNewTimedAction(args => Landing = false,
+                    _landingAction = RoutineBehaviour.Instance.StartNewTimedAction(args =>
+                        {
+                            Landing = false;
+                            CanCheckLanding = false;
+                        },
                         TimedActionCountType.SCALEDTIME, _landingTime);
                     break;
                 case AirState.BREAKINGFALL:
@@ -127,11 +146,18 @@ namespace Lodis.Movement
                     {
                         Landing = false;
                         _knockback.Physics.MakeKinematic();
+                        CanCheckLanding = false;
                     }, TimedActionCountType.SCALEDTIME, _knockback.DefenseBehaviour.GroundTechLength);
                     break;
             }
 
             _knockback.CancelHitStun();
+        }
+
+        private void Update()
+        {
+            if (CanCheckLanding && !Landing)
+                TryStartLandingLag();
         }
     }
 }
