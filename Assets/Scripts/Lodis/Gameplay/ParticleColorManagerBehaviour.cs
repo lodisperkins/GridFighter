6894ec375c7b21efcf38e882e872ee018f70ad98
Lodis <lodisperkins44@gmail.com>
2022-12-13 17:03:46 -0600

    Added new particle effects for ring barrier

---
+++ Assets/Scripts/Lodis/Gameplay/ParticleColorManagerBehaviour.cs
@@ -0,0 +1,56 @@
+﻿using Lodis.GridScripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lodis.Gameplay
+{
+    public class ParticleColorManagerBehaviour : MonoBehaviour
+    {
+        [SerializeField] 
+        private GridScripts.GridAlignment _alignment;
+        [SerializeField]
+        private bool _changeStartColor;
+        [SerializeField]
+        private bool _changeColorOverLifetime;
+        [SerializeField]
+        private bool _changeColorBySpeed;
+        [SerializeField]
+        private ParticleSystem[] _particleSystems;
+        private Color _color;
+
+        public GridAlignment Alignment { get => _alignment; set => _alignment = value; }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _color = BlackBoardBehaviour.Instance.GetPlayerColorByAlignment(Alignment);
+            SetColors();
+        }
+
+        public void SetColors()
+        {
+            foreach (ParticleSystem particleSystem in _particleSystems)
+            {
+
+                if (_changeStartColor)
+                {
+                    ParticleSystem.MainModule main = particleSystem.main;
+                    main.startColor = _color;
+                }
+
+                if (_changeColorOverLifetime)
+                {
+                    ParticleSystem.ColorOverLifetimeModule lifetimeModule = particleSystem.colorOverLifetime;
+                    lifetimeModule.color = _color;  
+                }
+
+                if (_changeColorBySpeed)
+                {
+                    ParticleSystem.ColorBySpeedModule speedModule = particleSystem.colorBySpeed;
+                    speedModule.color = _color; 
+                }
+            }
+        }
+    }
+}
