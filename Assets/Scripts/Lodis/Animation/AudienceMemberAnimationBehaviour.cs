4eb72618009015cf3e63804a9f6236bbf8368463
lodisperkins <lodisperkins44@gmail.com>
2022-12-18 15:29:29 -0600

    Added comments to audience animation and particle color scripts

--- Assets/Scripts/Lodis/Animation/AudienceMemberAnimationBehaviour.cs
+++ Assets/Scripts/Lodis/Animation/AudienceMemberAnimationBehaviour.cs
@@ -7,14 +7,19 @@ namespace Lodis
     public class AudienceMemberAnimationBehaviour : MonoBehaviour
     {
         [SerializeField]
+        [Tooltip("If true, the random scale will always be the same in all axis.")]
         private bool _uniformScale;
         [SerializeField]
+        [Tooltip("The smallest scale this can be in any axis.")]
         private float _minScale;
         [SerializeField]
+        [Tooltip("The largest scale this can be in any axis.")]
         private float _maxScale;
         [SerializeField]
+        [Tooltip("The slowest the audience animation is allowed to play.")]
         private float _minSpeed;
         [SerializeField]
+        [Tooltip("The fastest the audience animation is allowed to play.")]
         private float _maxSpeed;
         private Animator _animator;
         
@@ -22,14 +27,19 @@ namespace Lodis
         void Awake()
         {
             _animator = GetComponent<Animator>();
+
+            //Assign random values.
+
             _animator.speed = Random.Range(_minSpeed, _maxSpeed);
 
+            //Scale on all axis evenly if it's been enabled.
             if (_uniformScale)
             {
                 transform.localScale = new Vector3(Random.Range(_minScale, _maxScale), Random.Range(_minScale, _maxScale), Random.Range(_minScale, _maxScale));
                 return;
             }
 
+           
             float scale = Random.Range(_minScale, _maxScale);
 
             transform.localScale = new Vector3(scale, scale, scale);
--- Assets/Scripts/Lodis/Gameplay/ParticleColorManagerBehaviour.cs
+++ Assets/Scripts/Lodis/Gameplay/ParticleColorManagerBehaviour.cs
@@ -8,76 +8,101 @@ namespace Lodis.Gameplay
     public class ParticleColorManagerBehaviour : MonoBehaviour
     {
         [SerializeField] 
+        [Tooltip("The grid alignment that this particle will set its color to match.")]
         private GridScripts.GridAlignment _alignment;
         [SerializeField]
+        [Tooltip("If true, only the hue will be effected by the color change.")]
         private bool _onlyChangeHue;
+
+        [Header("Color Change Toggles")]
+
         [SerializeField]
         private bool _changeStartColor;
         [SerializeField]
         private bool _changeColorOverLifetime;
         [SerializeField]
         private bool _changeColorBySpeed;
+
+        [Space]
         [SerializeField]
+        [Tooltip("All of the particle effects that will have their colors changed. Mainly used to change children,but can be used to edit any particle effect.")]
         private ParticleSystem[] _particleSystems;
         private Color _color;
 
+        /// <summary>
+        /// The grid alignment that this particle will set its color to match.
+        /// </summary>
         public GridAlignment Alignment { get => _alignment; set => _alignment = value; }
 
         // Start is called before the first frame update
         void Start()
         {
+            //Initialize colors in start to be sure the alignment colors have been set up already.
             _color = BlackBoardBehaviour.Instance.GetPlayerColorByAlignment(Alignment);
             SetColors();
         }
 
-        public Color ChangeHue(Color oldColor)
+        /// <param name="oldColor">The current color of the property that is being changed.</param>
+        /// <returns>A color value that only matches the hue of the alignment color.</returns>
+        public Color GetHue(Color oldColor)
         {
             Color newColor = _color;
             Color propertyColor = oldColor;
             Vector3 propertyHSV = new Vector3();
             Vector3 targetHSV = new Vector3();
 
+            //Converts the current color and the desired color to HSV in order to get the hue value for both.
             Color.RGBToHSV(propertyColor, out propertyHSV.x, out propertyHSV.y, out propertyHSV.z);
             Color.RGBToHSV(newColor, out targetHSV.x, out targetHSV.y, out targetHSV.z);
 
+            //Set the hue of the current color to that of the alignment color.
             propertyHSV.x = targetHSV.x;
 
+            //Return the new color converted back to standard RGB format.
             newColor = Color.HSVToRGB(propertyHSV.x, propertyHSV.y, propertyHSV.z);
-
             return newColor;
         }
 
+        /// <summary>
+        /// Changes the color of each property that is allowed to be changed in each particle system.
+        /// </summary>
         public void SetColors()
         {
+            //Iterate through all systems to change the colors for each.
             foreach (ParticleSystem particleSystem in _particleSystems)
             {
-
+                //If allowed to change the start color...
                 if (_changeStartColor)
                 {
                     ParticleSystem.MainModule main = particleSystem.main;
 
+                    //...either just change the hue or the entire color.
                     if (_onlyChangeHue)
-                        main.startColor = ChangeHue(main.startColor.color);
+                        main.startColor = GetHue(main.startColor.color);
                     else
                         main.startColor = _color;
                 }
 
+                //If allowed to change the "ColorOverLifetime" color...
                 if (_changeColorOverLifetime)
                 {
                     ParticleSystem.ColorOverLifetimeModule lifetimeModule = particleSystem.colorOverLifetime;
 
+                    //...either just change the hue or the entire color.
                     if (_onlyChangeHue)
-                        lifetimeModule.color = ChangeHue(lifetimeModule.color.color);
+                        lifetimeModule.color = GetHue(lifetimeModule.color.color);
                     else
                         lifetimeModule.color = _color;
                 }
 
+                //If allowed to change the "ColorBySpeed" color...
                 if (_changeColorBySpeed)
                 {
                     ParticleSystem.ColorBySpeedModule speedModule = particleSystem.colorBySpeed;
 
+                    //...either just change the hue or the entire color.
                     if (_onlyChangeHue)
-                        speedModule.color = ChangeHue(speedModule.color.color);
+                        speedModule.color = GetHue(speedModule.color.color);
                     else
                         speedModule.color = _color;
                 }
