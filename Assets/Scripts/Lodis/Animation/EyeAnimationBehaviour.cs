98640199f7567427717924d6aaddbb1bb70cae82
lodisperkins <lodisperkins44@gmail.com>
2022-12-22 15:40:12 -0600

    Added eye animations

---
+++ Assets/Scripts/Lodis/Animation/EyeAnimationBehaviour.cs
@@ -0,0 +1,64 @@
+﻿using Lodis.Gameplay;
+using Lodis.Utility;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lodis.Animation
+{
+    public class EyeAnimationBehaviour : MonoBehaviour
+    {
+        [SerializeField]
+        private Renderer _renderer;
+        [SerializeField]
+        private string _textureName;
+
+        [SerializeField]
+        private float _timeBetweenBlinks;
+        [SerializeField]
+        private float _blinkDuration;
+
+        [SerializeField]
+        private Texture2D _idle;
+        [SerializeField]
+        private Texture2D _closed;
+        [SerializeField]
+        private Texture2D _hurt;
+
+        private bool _isBlinking;
+        private TimedAction _currentAction;
+
+        private void Blink()
+        {
+            _isBlinking = true;
+            _renderer.material.SetTexture(_textureName, _closed);
+
+            _currentAction = RoutineBehaviour.Instance.StartNewTimedAction(args =>
+            {
+                _renderer.material.SetTexture(_textureName, _idle);
+                _isBlinking = false;
+            }, TimedActionCountType.SCALEDTIME, _blinkDuration);
+
+        }
+
+        public void ChangeEyesToHurt()
+        {
+            RoutineBehaviour.Instance.StopAction(_currentAction);
+            _isBlinking = false;
+            _renderer.material.SetTexture(_textureName, _hurt);
+        }
+
+        public void ChangeEyesToIdle()
+        {
+            _isBlinking = false;
+            _renderer.material.SetTexture(_textureName, _idle);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if ((_currentAction == null || !_currentAction.GetEnabled()) && !_isBlinking)
+                _currentAction = RoutineBehaviour.Instance.StartNewTimedAction(args => Blink(), TimedActionCountType.SCALEDTIME, _timeBetweenBlinks);
+        }
+    }
+}
