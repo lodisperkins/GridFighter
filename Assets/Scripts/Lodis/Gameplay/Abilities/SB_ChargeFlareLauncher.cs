243866d75e3767c6a14c257d5eb031180599d5b9
Lodis <lodisperkins44@gmail.com>
2023-03-15 16:57:57 -0500

    Added charge flare launcher ability

---
+++ Assets/Scripts/Lodis/Gameplay/Abilities/SB_ChargeFlareLauncher.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lodis.Gameplay
+{
+
+    /// <summary>
+    /// Enter ability description here
+    /// </summary>
+    public class SB_ChargeFlareLauncher : ProjectileAbility
+    {
+	    //Called when ability is created
+        public override void Init(GameObject newOwner)
+        {
+			base.Init(newOwner);
+            ScaleStats = true;
+        }
+
+	    //Called when ability is used
+        protected override void OnActivate(params object[] args)
+        {
+            //The base activate func fires a single instance of the projectile when called
+            base.OnActivate(args);
+        }
+    }
+}
