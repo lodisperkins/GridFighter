093a353e46fba3d6cda1b0fb9eea9fba9c8530ac
lodisperkins <42353525+lodisperkins@users.noreply.github.com>
2023-04-17 22:37:16 -0500

    Added deck manager and new button

---
+++ Assets/Scripts/Lodis/UI/DeckBuildingManagerBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using Lodis.Gameplay;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lodis.UI
+{
+    public class DeckBuildingManagerBehaviour : MonoBehaviour
+    {
+        private Deck _deck;
+        
+        public void LoadDeck(string deckName)
+        {
+
+        }
+    }
+}
