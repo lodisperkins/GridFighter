da67a3fd1198bd04e39f4813f811a7f849c7eb2b
Lodis <lodisperkins44@gmail.com>
2023-06-05 18:51:13 -0500

    Updated glow properties for panels

--- Assets/Scripts/Lodis/GridScripts/PanelBehaviour.cs
+++ Assets/Scripts/Lodis/GridScripts/PanelBehaviour.cs
@@ -189,7 +189,10 @@ namespace Lodis.GridScripts
                 return;
 
             _mesh?.material.ChangeHue(_defaultColor, "_Color");
-            _mesh?.material.SetFloat("_EmissionStrength", _emissionStrength / 2);
+
+            //if (_mesh.material.GetFloat("_EmissionStrength") > 0)
+            //    _mesh?.material.SetFloat("_EmissionStrength", _emissionStrength / 2);
+                
             _mesh?.material.DOFloat(0, "_EmissionStrength", _emissionFadeDuration);
             CurrentMarker = MarkerType.NONE;
         }
