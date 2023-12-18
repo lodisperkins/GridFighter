8c85e315fcf56cabe1eb37328c9fb2a05b43f190
lodisperkins <lodisperkins44@gmail.com>
2023-12-17 20:58:53 -0600

    Added training menu option and submenu

---
+++ Assets/Scripts/Lodis/Gameplay/TrainingBackgroundBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using Lodis.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Lodis.Gameplay
+{
+
+    public class TrainingBackgroundBehaviour : MonoBehaviour
+    {
+        private BackgroundColorBehaviour _bgColor;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _bgColor = GetComponent<BackgroundColorBehaviour>();
+            _bgColor.SetPrimaryColor(BlackBoardBehaviour.Instance.Player1Color);
+            _bgColor.SetSecondaryColor(BlackBoardBehaviour.Instance.Player2Color);
+        }
+    }
+}
